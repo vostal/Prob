5ff0dac0776119e983497b8326c8f4354ec01b6b
vostal <neboxitel@gmail.com>
2022-06-01 14:09:18 +0400

    All new

---
+++ Prob/Prob/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
